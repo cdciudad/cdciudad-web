9ac988029a7663d08a4726b2b6cb5840f4587f1a
Olman Francisco Castro Hernández <kstro379@gmail.com>
2022-03-04 17:19:37 -0600

    [update] Refactor core structure

--- cdciudad-web/src/app/app-routing.module.ts
+++ cdciudad-web/src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './core/pages/main/main.component';
-import { ServicesSelectComponent } from './service-offer/pages/services-select/services-select.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
-    pathMatch: 'full'
+    children: [
+      {
+        path: 'historia',
+        loadChildren: () =>
+          import('./history/history.module').then((m) => m.HistoryModule),
+      },
+    ],
   },
-  {
-    path: 'servicios',
-    component: ServicesSelectComponent
-  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
