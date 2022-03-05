7166c11eac6f5ba566fbe213b0c85524cb76460c
Olman Francisco Castro Hernández <kstro379@gmail.com>
2022-03-05 00:04:01 -0600

    [add] History page HTML & CSS

---
+++ cdciudad-web/src/app/history/components/card-message/card-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-message',
+  templateUrl: './card-message.component.html',
+  styleUrls: ['./card-message.component.scss']
+})
+export class CardMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- cdciudad-web/src/app/history/history.module.ts
+++ cdciudad-web/src/app/history/history.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HistoryRoutingModule } from './history-routing.module';
 
 import { HistoryComponent } from './pages/history/history.component';
+import { CardMessageComponent } from './components/card-message/card-message.component';
 
 @NgModule({
-  declarations: [HistoryComponent],
+  declarations: [HistoryComponent, CardMessageComponent],
   imports: [CommonModule, HistoryRoutingModule],
 })
 export class HistoryModule {}
