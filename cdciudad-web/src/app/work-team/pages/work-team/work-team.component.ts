26519cafb397fdc5102c30cb3f2df0ca17c53ff4
Olman Francisco Castro Hernández <kstro379@gmail.com>
2022-03-19 10:59:09 -0600

    [add] Work team responsive styles

--- cdciudad-web/src/app/work-team/pages/work-team/work-team.component.ts
+++ cdciudad-web/src/app/work-team/pages/work-team/work-team.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-work-team',
   templateUrl: './work-team.component.html',
-  styleUrls: ['./work-team.component.scss']
+  styleUrls: ['./work-team.component.scss'],
 })
 export class WorkTeamComponent implements OnInit {
+  @ViewChild('dropDown') dropDown!: ElementRef;
+  @ViewChild('dropDownFocus') dropDownFocus!: ElementRef;
 
-  constructor() { }
+  dropDownState: boolean = false;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  open() {
+    if (this.dropDownState) {
+      this.dropDown.nativeElement.style.marginBottom = '30px';
+      this.dropDownState = false;
+    } else {
+      this.dropDown.nativeElement.style.marginBottom = '180px';
+      this.dropDownState = true;
+    }
   }
-
 }
