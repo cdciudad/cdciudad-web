719f4fc2392a1f71f11e94058bc75c6623a3f54b
Olman Francisco Castro Hernández <kstro379@gmail.com>
2022-03-19 14:42:26 -0600

    [add] Profile mobile view

--- cdciudad-web/src/app/work-team/work-team-routing.module.ts
+++ cdciudad-web/src/app/work-team/work-team-routing.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { WorkTeamComponent } from './pages/work-team/work-team.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: WorkTeamComponent,
+  },
+  {
+    path: ':id',
+    component: ProfileComponent,
   },
 ];
 
