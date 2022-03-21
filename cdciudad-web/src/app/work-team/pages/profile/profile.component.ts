1b1c694b8328512347bd1209844d3216af798a23
Olman Francisco Castro Hernández <kstro379@gmail.com>
2022-03-20 21:06:19 -0600

    [add] Temporal Slider

--- cdciudad-web/src/app/work-team/pages/profile/profile.component.ts
+++ cdciudad-web/src/app/work-team/pages/profile/profile.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  @ViewChild('slides_container') displace!: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  scrollLeftTo(): void {
+    this.displace.nativeElement.scrollBy(-2, 0);
   }
 
+  scrollRightTo(): void {
+    this.displace.nativeElement.scrollBy(10, 0);
+  }
 }
