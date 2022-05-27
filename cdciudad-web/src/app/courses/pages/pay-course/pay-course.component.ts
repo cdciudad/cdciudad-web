e233c43eb2da3eae0b48713b8f37521b6c1ab344
Olman Francisco Castro Hernández <kstro379@gmail.com>
2022-05-27 12:17:39 -0600

    [add] Pay courses view

---
+++ cdciudad-web/src/app/courses/pages/pay-course/pay-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pay-course',
+  templateUrl: './pay-course.component.html',
+  styleUrls: ['./pay-course.component.scss']
+})
+export class PayCourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ cdciudad-web/src/app/courses/pages/pay-enrollment/pay-enrollment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pay-enrollment',
+  templateUrl: './pay-enrollment.component.html',
+  styleUrls: ['./pay-enrollment.component.scss']
+})
+export class PayEnrollmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
