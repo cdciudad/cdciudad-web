8aee7c8fa35efde88f039e6feba210485d7e317c
Olman Francisco Castro Hernández <kstro379@gmail.com>
2022-05-27 13:36:16 -0600

    [fix] Scroll and zindex header menu

--- cdciudad-web/src/app/courses/components/pay/main-banner/main-banner.component.ts
+++ cdciudad-web/src/app/courses/components/pay/main-banner/main-banner.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-banner',
   templateUrl: './main-banner.component.html',
-  styleUrls: ['./main-banner.component.scss']
+  styleUrls: ['./main-banner.component.scss'],
 })
 export class MainBannerComponent implements OnInit {
+  @Input() titleOne: string = '';
+  @Input() titleTwo: string = '';
+  @Input() description: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- cdciudad-web/src/app/courses/pages/pay-enrollment/pay-enrollment.component.ts
+++ cdciudad-web/src/app/courses/pages/pay-enrollment/pay-enrollment.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pay-enrollment',
   templateUrl: './pay-enrollment.component.html',
-  styleUrls: ['./pay-enrollment.component.scss']
+  styleUrls: ['./pay-enrollment.component.scss'],
 })
 export class PayEnrollmentComponent implements OnInit {
+  titleOne: string = 'Pagá tu';
+  titleTwo: string = 'matrícula';
+  description: string =
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum amet labore minima suscipit. Deleniti aspernatur ab expedita quis saepe dolores, nisi id cumque tempora debitis maiores voluptates non illo suscipit?';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
