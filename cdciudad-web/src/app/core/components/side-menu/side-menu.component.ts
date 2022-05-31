fe3349fe374c26ada5c1df0c503ca91030c4cf68
Olman Francisco Castro Hernández <kstro379@gmail.com>
2022-05-30 22:22:00 -0600

    [add] Side menu courses deep menu

--- cdciudad-web/src/app/core/components/side-menu/side-menu.component.ts
+++ cdciudad-web/src/app/core/components/side-menu/side-menu.component.ts
@@ -7,10 +7,16 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class SideMenuComponent {
   @ViewChild('checkBox') menuState!: ElementRef;
+  coursesMenu: boolean = false;
 
   constructor() {}
 
   closed(): void {
     this.menuState.nativeElement.checked = false;
+    this.coursesMenu = false;
+  }
+
+  showCoursesMenu(): void {
+    this.coursesMenu = !this.coursesMenu;
   }
 }
